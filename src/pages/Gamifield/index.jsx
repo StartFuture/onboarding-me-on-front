9b66a143aaa1c1a11f5946c0ac65ee94a3ec31c8
IsabellajFerreira <isabellajferreira@hotmail.com>
2023-08-04 20:04:36 -0300

    Structuring the welcome presentation page

--- src/pages/Gamifield/index.jsx
+++ src/pages/Gamifield/index.jsx
@@ -1,5 +1,27 @@
+import React from "react";
+
+
 const Gamifield = () => {
- return <h1>Gamificacao</h1>;
+ return(
+
+    <div className="video-screen-container">
+      <div className="title-with-bar">
+        <div className="bar"></div>
+        <h1 className="title">
+          Olá, Usuário. <br/>
+          Boas-vindas à Jornada Gamificada da EmpresaNova.
+        </h1>
+        <p>Para iniciar, assista o vídeo que preparamos para você. </p>
+      </div>
+      <iframe
+        className="video-iframe"
+        src="https://www.youtube.com/embed/2VsENIzqWq8"
+        title="Vídeo do Youtube"
+        allowFullScreen
+      ></iframe>
+      <button className="button">Continuar</button>
+    </div>
+ ); 
 };
 
 export default Gamifield;
