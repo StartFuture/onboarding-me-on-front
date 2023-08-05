7331e9f7a957d530c33d87a5eeeebcbd8ff95171
IsabellajFerreira <isabellajferreira@hotmail.com>
2023-08-04 21:59:27 -0300

    styling the page

--- src/pages/Gamifield/index.jsx
+++ src/pages/Gamifield/index.jsx
@@ -1,26 +1,30 @@
 import React from "react";
-
+import "./style.css"
 
 const Gamifield = () => {
  return(
 
-    <div className="video-screen-container">
+    <div className="video-container">
       <div className="title-with-bar">
         <div className="bar"></div>
         <h1 className="title">
           Olá, Usuário. <br/>
           Boas-vindas à Jornada Gamificada da EmpresaNova.
         </h1>
-        <p>Para iniciar, assista o vídeo que preparamos para você. </p>
-      </div>
+         </div>
+         <p>Para iniciar, assista o vídeo que preparamos para você. </p>
+     
       <iframe
         className="video-iframe"
         src="https://www.youtube.com/embed/2VsENIzqWq8"
         title="Vídeo do Youtube"
         allowFullScreen
       ></iframe>
+
+     <div className="continue-button">
       <button className="button">Continuar</button>
-    </div>
+      </div>
+      </div>
  ); 
 };
 
