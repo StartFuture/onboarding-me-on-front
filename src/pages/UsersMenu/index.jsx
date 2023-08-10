d00a58c5a427e117f31a37618152db61b7552aa9
Igor <igornteixeira48@gmail.com>
2023-08-09 23:38:37 -0300

    responsive

--- src/pages/UsersMenu/index.jsx
+++ src/pages/UsersMenu/index.jsx
@@ -1,6 +1,11 @@
 import {useState} from 'react';
 import Navbar from '../../components/Navbar';
 import SubTitle from '../../components/subtitle';
+import Edicao from '../../assets/svg/Edição.svg'
+import Youtube from '../../assets/svg/Youtube.svg'
+import Lixeira from '../../assets/svg/Lixeira.svg'
+
+
 import { AdminMenu, ContentContainer, FerramentasEdicao, MainContent, Navigation, NavigationButton, Ferramentas  } from './styles.ts'
 import Sidebar from '../../components/sidebar/Sidebar';
 
@@ -30,10 +35,10 @@ export default function UsersMenu() {
           </Navigation>
           <SubTitle>Olá, Usuário. Configure a Jornada Gamificada dos seus colaboradores.</SubTitle>
           <Ferramentas>
-            <img src="youtube" alt="logo youtube" />
+            <img src={Youtube} alt="logo youtube" />
             <p>youtube link</p> 
-            <img src="" alt="editar" />
-            <img src="" alt="remover" />
+            <img src={Edicao} alt="editar" />
+            <img src={Lixeira} alt="remover" />
             </Ferramentas>
             <div className="Ferramentas">
             <FerramentasEdicao />
