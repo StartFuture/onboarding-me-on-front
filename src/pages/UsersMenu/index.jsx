c8efe4281c2d99f5dfe2d7ac334fa7203ed5cbbf
Igor <igornteixeira48@gmail.com>
2023-08-11 09:19:10 -0300

    css

--- src/pages/UsersMenu/index.jsx
+++ src/pages/UsersMenu/index.jsx
@@ -1,4 +1,5 @@
 import {useState} from 'react';
+import './styles.css'
 import Navbar from '../../components/Navbar';
 import Sidebar from '../../components/sidebar/Sidebar';
 import SubTitle from '../../components/subtitle';
@@ -6,10 +7,7 @@ import Edicao from '../../assets/svg/Edição.svg'
 import Youtube from '../../assets/svg/Youtube.svg'
 import Lixeira from '../../assets/svg/Lixeira.svg'
 import Discord from '../../assets/svg/Discord.svg'
-import { AdminMenu,Stick, ContentContainer, MainContent, Navigation, NavigationButton, Ferramentas, RedButton, Alinhar, EdicaoFerramentas } from './styles.ts'
-import Home from '../Minigame';
-
-
+s
 
 export default function UsersMenu() {
   const [isActive, setIsActive] = useState(false)
@@ -19,55 +17,18 @@ export default function UsersMenu() {
   } 
 
   return (
-    <AdminMenu>
+    <>
       <Navbar />
       <Sidebar />
-      <ContentContainer>
-        <MainContent>
-          <Stick/>
-          <SubTitle>Olá, Usuário. Configure a Jornada Gamificada dos seus colaboradores.</SubTitle>
-          <Navigation>
-            <NavigationButton onClick={handleIsActive} active={isActive}>    GERAL
-            </NavigationButton> 
-            <NavigationButton onClick={handleIsActive} active={isActive}>
-              CULTURA
-            </NavigationButton> 
-            <NavigationButton onClick={handleIsActive} active={isActive}>
-              PRINCÍPIOS
-            </NavigationButton>
-          </Navigation>
-          <SubTitle>Informe o vídeo de apresentação da empresa. O colaborador irá assistir assim que iniciar o processo..</SubTitle>
-          <Ferramentas>
-            <img src={Youtube} alt="logo youtube"  />
-            <SubTitle>youtube link</SubTitle> 
-            <img src={Edicao} alt="editar" className='' />
-            <img src={Lixeira} alt="remover" />
-          </Ferramentas>
-          <Alinhar>
-            <SubTitle>Ferramentas do dia a dia.</SubTitle>
-            <RedButton>Adicionar Ferramnetas</RedButton>
-          </Alinhar>
-          <EdicaoFerramentas>
-            <FerramentasEdicao>
-              <img src={Discord} alt="Discord" />
-              <SubTitle>Discord</SubTitle>
-              <img src={Edicao} alt="" />
-              <img src={Lixeira} alt="" />
-            </FerramentasEdicao>
-            <FerramentasEdicao>
-              <img src={Discord} alt="Discord" />
-              <SubTitle>Discord</SubTitle>
-              <img src={Edicao} alt="" />
-              <img src={Lixeira} alt="" />
-            </FerramentasEdicao> 
-            <FerramentasEdicao>
-              <img src={Discord} alt="Discord" />
-              <SubTitle>Discord</SubTitle>              <img src={Edicao} alt="" />
-              <img src={Lixeira} alt="" />
-            </FerramentasEdicao>  
-          </EdicaoFerramentas>
-        </MainContent>
-      </ContentContainer>
-    </AdminMenu>
-  );    
+      <div className='Maincontent'>
+        <div className='Title-with-Bar'>
+          <div className='Bar' />
+          <div className='Title'>
+            <h3>Olá, Usuário.<br/>
+              Configure a Jornada Gamificada dos seus colaboradores.</h3>
+          </div>
+        </div>
+      </div>
+    </>
+  );  
 };
