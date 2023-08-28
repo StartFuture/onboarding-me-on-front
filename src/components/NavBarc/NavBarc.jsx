d188f2dc4df701765a3ecafa3537c98ef7ca2043
Igor <igornteixeira48@gmail.com>
2023-08-28 17:07:36 -0300

    fix(layout): sidebar e navbar funcionando

--- src/components/NavBarc/NavBarc.jsx
+++ src/components/NavBarc/NavBarc.jsx
@@ -1,21 +1,19 @@
 import React from 'react';
-import './NavBarc.css';
+import styles from "./styles.module.css"
 import userIcon from "../../assets/userIcon.png"
 import BellIcon from "../../assets/notification-bell-svgrepo-com.svg"
 
 export default function NavBarc(){
 
 return(
+    <div className={styles.NavbarContainer}>
+        <div className={styles.BellIconDiv}>
+            <img src={BellIcon} />
+        </div>
 
-    <header className={styles.LayoutHeader}>
-                    <div className={styles.BellIconDiv}>
-                        {/* a tag img precisa do atributo src para inserir a img */}
-                        <img src={BellIcon}/>
-                    </div>
+        <p>Usuário Logado</p>
 
-                    <p>Usuário Logado</p>
-
-                    <img src={userIcon} className={styles.userIcon}/>
-    </header>
+        <img src={userIcon} className={styles.userIcon} />
+    </div>
 )
 }
--- src/components/SideBarc/SideBarc.jsx
+++ src/components/SideBarc/SideBarc.jsx
@@ -1,28 +1,21 @@
 import {Outlet,NavLink} from "react-router-dom"
-import styles from "./SideBarc.css"
+import styles from "./Sidebar.module.css"
+import Logo from "../../assets/svg/Logo.svg"
 
 export default function SideBarc() {
 
 return(
-    <main className={styles.layoutMainContainer}>
 
-                    {/* tag do html semantica para conteudo lateral */}
-                    <aside className={styles.layoutSidebar}>
-                        <h2>O/ME</h2>
+    <div className={styles.SidebarContainer}>
+        <div className={styles.logoContainer}>
+            <img src={Logo} alt="O/ME Logo" className={styles.logoImage}/>
+        </div>
+        <nav className={styles.SidebarNavLinks}>
+            <NavLink to="/menu"><p>Jornada Gamificada</p></NavLink>
+            <NavLink to="/welcomekit"><p>Welcome Kit</p></NavLink>    
+        </nav>
+    </div>
 
-                        {/* tag do html semantica para links navegaveis */}
-                        <nav className={styles.layoutSidebarNavlinksNav}>
 
-                            {/* elemento importado da biblioteca de rotiamento utilizado para navegar entre rotas */}
-                            <NavLink to="/"><p>Jornada Gamificada</p></NavLink>
-                            <NavLink to="/welcomekit"><p>Welcome Kit</p></NavLink>    
-                        </nav>
-                        
-                    </aside>
-
-                    {/* esse é o elemento rederizador das rotas que estão dentro desse layout  */}
-                    <Outlet/>
-
-                </main>
 )
 }
