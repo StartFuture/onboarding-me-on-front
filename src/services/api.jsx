0cf9298d714bef33dfbaa9f6ac3ebdd211f95f82
Ian Soares <80067490+Ian-Soares@users.noreply.github.com>
2023-09-23 18:06:57 +0000

    Update api.jsx

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://127.0.0.1:8000/"
+    baseURL: "https://api.onboarding-on.startfuture.com.br/"
 })
 
 export default api;
